33d9710c5ca6216436f306b30b5c0b8892c8c7e9
Gabriel Dayley <gabriel.dayley@gmail.com>
2014-11-25 21:20:17 -0700

    refactored rocket

--- pirocket.js
+++ pirocket.js
@@ -10,50 +10,84 @@ function Rocket(opts) {
     dataInterval: 100,
     mode: 1,
     servoInitAngle: 150,
-    servoReleaseAngle: 65,
-    armAltDelta: 8,
-    deployAltDelta: 2,
-    autoResetDelay: 30000
+    servoReleaseAngle: 65
   }, opts);
 
   var thiz = this;
   var altBuffer = [];
+
+  var data = {
+    launched: false,
+    deployed: false
+  };
 
   var robot = Cylon.robot({
     connection: {name: 'raspi', adaptor: 'raspi'},
     devices: [
       { name: 'bmp180',   driver: 'bmp180' },
       { name: 'servo',    driver: 'servo',  pin: 12 },
-      { name: 'statusLed', driver: 'led',    pin: 15 }
+      { name: 'statusLed', driver: 'led',    pin: 15 },
+      { name: 'statusLed2', driver: 'led',    pin: 18 },
+      { name: 'btn1', driver: 'button',    pin: 11 },
+      { name: 'btn2', driver: 'button',    pin: 16 }
     ],
 
     work: function(my) {
       my.statusLed.turnOn();
 
-      every(config.dataInterval, function() {
+      my.btn1.on('push', function() {
+        //console.log('BUTTON 1 PUSHED');
+        thiz.emit('btn1-pushed');
+        robot.armParachute();
+      });
+
+      my.btn2.on('push', function() {
+        console.log('BUTTON 2 PUSHED');
+        thiz.emit('btn2-pushed');
+      });
+
+      // Read sensor data
+      every(100, function() {
         my.bmp180.getAltitude(1, null, function(err, values) {
           if(err){ console.log(err) }
           else {
-            thiz.emit('data', values);
+            data.temperature = values.temp;
+            data.pressure = values.press;
+            data.altitude = values.alt;
 
             // Detect launch
-            altBuffer.push(values.alt);
-            if(altBuffer.length > 5) {
-              altBuffer = altBuffer.slice(-5);
-              if(altBuffer[4] - altBuffer[0] > 2) {
-                thiz.emit('launch');
+            if(!data.launched) {
+              altBuffer.push(values.alt);
+              if (altBuffer.length > 10) {
+                altBuffer = altBuffer.slice(-10);
+                if (altBuffer[9] - altBuffer[0] > 2) {
+                  data.launched = true;
+                  thiz.emit('launch');
+                }
               }
             }
           }
-        })
+        });
+      });
+
+      // Emit sensor data
+      every(config.dataInterval, function() {
+        thiz.emit('data', data);
       });
     },
 
     armParachute: function() {
       robot.servo.angle(config.servoInitAngle);
+      robot.statusLed2.turnOn();
+      data.launched = data.deployed = false;
+      thiz.emit('parachute-armed');
     },
+
     deployParachute: function() {
       robot.servo.angle(config.servoReleaseAngle);
+      robot.statusLed2.turnOff();
+      data.deployed = true;
+      thiz.emit('parachute-deployed');
     }
   });
 
